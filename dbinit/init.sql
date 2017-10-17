2ac58a1a5ecef9434d509a95336c01a683f4383d
chencheng <chencheng@ymatou.com>
2017-10-17 11:30:34 +0800

    dbinit

---
+++ dbinit/init.sql
@@ -0,0 +1,18 @@
+SET FOREIGN_KEY_CHECKS=0;
+CREATE DATABASE IF NOT EXISTS SpAppDb DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+-- ----------------------------
+-- Table structure for searchrecoder
+-- ----------------------------
+DROP TABLE IF EXISTS `searchrecoder`;
+CREATE TABLE `searchrecoder` (
+  `reqid` int(11) NOT NULL auto_increment,
+  `reqloc` varchar(255) default NULL,
+  `reqkeywds` varchar(255) default NULL,
+  `remark` text,
+  `recommendsite` varchar(255) default NULL,
+  PRIMARY KEY  (`reqid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of searchrecoder
+-- ----------------------------
